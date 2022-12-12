5608a5d244c0007086514aadcc654b65a0c7ee99
Bas <78301501+Blasix@users.noreply.github.com>
2022-12-12 17:40:29 +0000

    basic tailwind and next.js setup

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+// Design: https://dribbble.com/shots/18719635-Ahensy-Creative-Design-Agency-Landing-Page-Website
+// Help: https://www.youtube.com/watch?v=k-Pi5ZMxHWY
+// Command to start: npm run dev
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Blasix</title>
+        <meta name="description" content="The official Blasix website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+    </div>
+  )
+}
