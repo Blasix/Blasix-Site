d12931ccb7574ed87ae83967cb605e68d31d8e05
Bas <78301501+Blasix@users.noreply.github.com>
2022-12-15 16:15:46 +0000

    made navbar better and fixed some things

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head'
+import { useState } from 'react';
 import { BsFillMoonStarsFill } from "react-icons/bs";
 
 // Design: https://dribbble.com/shots/18719635-Ahensy-Creative-Design-Agency-Landing-Page-Website
-// Help: https://www.youtube.com/watch?v=k-Pi5ZMxHWY
+// SVG: https://creazilla.com/nodes/1998602-startup-svg-clipart
 // Command to start: npm run dev
 
 export default function Home() {
+  const [toggle, setToggle] = useState(false);
   return (
     <div>
       <Head>
@@ -15,43 +18,58 @@ export default function Home() {
       </Head>
 
       <main className='bg-white px-10'>
-        <section className='min-h-screen'>
-          <nav className='py-10 mb-12 flex justify-between text-2xl lg:text-3xl'>
-            <h1 className='text-2xl md:text-3xl lg:text-6xl font-quicksandB text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-800'>Blasix</h1>
-            <ul className='hidden gap-4 lg:gap-8  items-center font-quicksand md:flex '>
-              <a href='#' >Services</a>
-              <a href='#' >About</a>
-              <a href='#' >My Projects</a>
-              <a href='#' >Insight</a>
-            </ul>
-            <a href='#' className='hidden px-4 py-2 font-quicksand md:flex'>Contact Me</a>
-            <a href='#' className='px-4 py-2 text-xl font-quicksand md:hidden'>Menu</a>
-            {/* if screen is small then show the menu icon */}
 
-          </nav>
-          <div className='text-center md:text-start'>
-            <h2 className='text-5xl  py-8 md:pl-10 font-quicksandB md:text-7xl'>Mobile<br />Development</h2>
-            <h3 className='font-quicksand md:py-6 md:pl-20 md:text-3xl'>Blasix is a company made by Bas,<br />to create the best innovations &<br />world class quality solutions</h3>
-            <ul className='flex py-6 md:pl-20 items-center font-quicksandB md:text-5xl' >
-              <h1 >Lets Talk </h1>
-              <h1 className='text-blue-700 px-4'> →</h1>
-            </ul>
+        <nav className='w-full flex py-6 mb-12 justify-between items-center text-2xl lg:text-3xl'>
+          <h1 className='text-3xl md:text-4xl lg:text-6xl font-quicksandB text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-800'>Blasix</h1>
+          <ul className='hidden gap-4 lg:gap-8 list-none items-center font-quicksand md:flex '>
+            <a href='#about' >About Us</a>
+            <a href='#projects' >Our Projects</a>
+          </ul>
+          <a href='/contact' className='hidden px-4 py-2 font-quicksand md:flex'>Contact Me</a>
+          <div className='md:hidden flex flex-1 justify-end items-center'>
+            <img
+              src={toggle ? 'close.svg' : 'menu.svg'}
+              alt='menu'
+              className='w-7 h-7 object-contain'
+              onClick={() => setToggle((prev) => !prev)} />
+            <div
+              className={`${!toggle ? "hidden" : "flex"
+                } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl bg-gradient-to-br from-gray-100 to-gray-300 sidebar`}>
+              <ul className=' gap-4 lg:gap-8 list-none items-center flex-col font-quicksand flex '>
+                <a href='#about' >About Us</a>
+                <a href='#projects' >Our Projects</a>
+                <a href='/contact' >Contact Me</a>
+              </ul>
+
+            </div>
+          </div>
+
+        </nav>
+        <section className='min-h-screen'>
+          <div className='text-start'>
+            <h2 className='text-5xl py-8 md:pl-10 font-quicksandB md:text-7xl'>Mobile<br />Development</h2>
+            <h3 className='font-quicksand md:py-6 md:pl-20 text-xl md:text-3xl'>Blasix is a company made by Bas,<br />to create the best innovations &<br />world class quality solutions</h3>
+            <li className='flex py-6 md:pl-20 items-center font-quicksandB text-xl md:text-5xl' >
+              <h1>Lets Talk </h1>
+              <h1 className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-800 px-4'> →</h1>
+            </li>
+            {/* <img src="Main.svg" alt="Main" /> */}
           </div>
         </section>
       </main>
 
-      {/* <footer className='bg-gray-100 py-8 px-10'>
-        <ul className='flex px-4 py-2 items-center text-6xl font-quicksandB'>
+      <footer className='bg-gray-100 py-8 px-4 md:px-10'>
+        <li className='flex pb-2 lg:pb-4 items-center text-3xl md:text-4xl lg:text-6xl font-quicksandB'>
           <h1 >Knock@Blasix.com </h1>
-          <h1 className='text-blue-700 px-4'> →</h1>
-        </ul>
-        <ul className='flex items-center font-quicksand text-xl'>
-          <a href='#' className='px-4 py-4'>Services</a>
-          <a href='#' className='px-4 py-4'>About</a>
-          <a href='#' className='px-4 py-4'>My Projects</a>
-          <a href='#' className='px-4 py-4'>Insight</a>
-        </ul>
-      </footer> */}
+          <h1 className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-800 px-4'> →</h1>
+        </li>
+        <li className='flex items-center font-quicksand text-xl lg:text-2xl gap-4 lg:gap-8'>
+          <a href='#'>About</a>
+          <a href='#'>Services</a>
+          <a href='#'>My Projects</a>
+          <a href='#'>Insight</a>
+        </li>
+      </footer>
 
     </div>
   )
